afa0b175e40a2f650fd42d98aa1ae56a47562dc3
= <karel_verhoeven@live.com>
2023-09-14 15:26:49 +0100

    writing to iteneraries

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_migrate import Migrate
 from helpers.itenerary import get_itenerary, load_config
 from database import db
 from models import Itenerary, UniqueSearchHistory
-from contracts.model_contracts import UniqueSearchHistorySchema
+from contracts.model_contracts import UniqueSearchHistorySchema, ItenerarySchema
+from sqlalchemy.exc import IntegrityError
 
 
 
@@ -23,11 +24,18 @@ def list_tables():
     
     return table_names
 
-@app.route("/query")
-def query():
+@app.route("/query_ush")
+def query_ush():
     data = db.session.query(UniqueSearchHistory).all()
     for item in data:
         print(item.id, " ", item.num_days, " ",item.country ," ", item.specific_places)
+    return "done"
+
+@app.route("/query_i")
+def query_i():
+    data = db.session.query(Itenerary).all()
+    for item in data:
+        print(item.id, " ", item.unique_search_history_id, " ",item.day ," ", item.morning_activity)
     return "done"
 
 
@@ -42,17 +50,48 @@ def home():
         from_date_obj = datetime.strptime(request.form["from"], '%Y-%m-%d')
         to_date_obj = datetime.strptime(request.form["to"], '%Y-%m-%d')
         days = (to_date_obj - from_date_obj).days 
-        ush = UniqueSearchHistorySchema(num_days=days, country=country, specific_places=region_string)
-        if ush.validate_data:  # Custom validation function
-            data_dict = ush.to_dict()
-            db_data = UniqueSearchHistory(**data_dict)
-            db.session.add(db_data)
-            db.session.commit()
-        else:
-            print("fuck")
+        unique_search_history_schema = UniqueSearchHistorySchema(num_days=days, country=country, specific_places=region_string)
+        if unique_search_history_schema.validate_data:  # Custom validation function
+            data_dict = unique_search_history_schema.to_dict()
+            unique_search_history_data = UniqueSearchHistory(**data_dict)
+            try: 
+                db.session.add(unique_search_history_data)
+                db.session.commit()
+                print(unique_search_history_data.id)
+            except IntegrityError as e:
+                print("Data already in the DB, lets fetch it from the table")
+                return "You need to pull the data from the DB dude"
+            df = get_itenerary(country, region_string, days, config)  #need to change this to not use a data frame and just keep the json
+            columns = ['index','Travel Method','Travel Time', 'Morning Activity','Afternoon Activity','Evening Activity']
+            itenerary_store = df[columns].copy()
+            itenerary_store['unique_search_history_id'] = unique_search_history_data.id
+            itenerary_store.columns = ['day','travel_method','travel_time','morning_activity','afternoon_activity','evening_activity','unique_search_history_id']
+            for index, row in itenerary_store.iterrows():
+                itenerary_schema = ItenerarySchema(
+                    unique_search_history_id = row['unique_search_history_id'],
+                    day = row['day'],
+                    travel_method = row['travel_method'],
+                    travel_time = row['travel_time'],
+                    morning_activity = row['morning_activity'],
+                    afternoon_activity = row['afternoon_activity'],
+                    evening_activity = row['evening_activity'])
+                if itenerary_schema.validate_data:  # Custom validation function
+                    data_dict = itenerary_schema.to_dict()
+                    itenerary_data = Itenerary(**data_dict)
+                    try: 
+                        db.session.add(itenerary_data)
+                        db.session.commit()
+                        print(itenerary_data.id)
+                    except IntegrityError as e:
+                        print("Data already in the DB, lets fetch it from the table")
 
-        df = get_itenerary(country, region_string, days, config)  #need to change this to not use a data frame and just keep the json
-        return render_template("itenerary.html",  tables=[df.to_html(classes='data', header="true")], df=df)
+            print("stored to itenerary")
+            return render_template("itenerary.html",  tables=[df.to_html(classes='data', header="true")], df=df)
+
+        else:
+            print("data not valid")
+
+
 
     tables = list_tables()
     print("Tables in the database:", tables)
--- contracts/model_contracts.py
+++ contracts/model_contracts.py
@@ -17,3 +17,27 @@ class UniqueSearchHistorySchema:
         if len(data.num_days) < 1:
             return False
         return True
+
+@dataclass
+class ItenerarySchema:
+    unique_search_history_id: int
+    day: str
+    travel_method: str
+    travel_time: str
+    morning_activity: str
+    afternoon_activity: str
+    evening_activity: str
+
+    def to_dict(self):
+        return {
+            'unique_search_history_id': self.unique_search_history_id,
+            'day': self.day,
+            'travel_method': self.travel_method,
+            'travel_time': self.travel_time,
+            'morning_activity': self.morning_activity,
+            'afternoon_activity': self.afternoon_activity,
+            'evening_activity': self.evening_activity
+        }
+
+    def validate_data(data):
+        return True
