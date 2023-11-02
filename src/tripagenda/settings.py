a58932dffc9a72e0521f97ed6a15f9d25a7fdb52
Coetzee van Staden <coetzee.vs@gmail.com>
2023-11-02 10:47:32 +0000

    update: strip more unnecessary things

--- src/tripagenda/settings.py
+++ src/tripagenda/settings.py
@@ -29,8 +29,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'api'
 ]
 
@@ -67,5 +65,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'api.User'
