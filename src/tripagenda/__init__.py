415b2138e9fdb5a7bbc172e2acfcb071acfb0120
Coetzee van Staden <coetzee.vs@gmail.com>
2023-11-04 14:28:23 +0000

    update: get local running

--- src/tripagenda/__init__.py
+++ src/tripagenda/__init__.py
@@ -1,9 +1,10 @@
 """Package initialization."""
 import logging
+import os
 
 from api.clients.openai import OpenAI
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tripagenda.settings")
 logger = logging.getLogger("tripagenda")
 oai = OpenAI()
 
--- src/tripagenda/asgi.py
+++ src/tripagenda/asgi.py
@@ -16,7 +16,8 @@ application = get_asgi_application()
 from django.conf import settings
 from fastapi import FastAPI
 
-from api.routers.city import router as city_router
+from api.routers.city import city_router
+from api.routers.city import cities_router
 from api.routers.itinerary import router as itinerary_router
 from api.routers.search import router as search_router
 from tripagenda.routers import router as internal_router
@@ -33,6 +34,7 @@ api_prefix = f"{settings.API_PREFIX}/{settings.API_VERSION}"
 internal_prefix = f"{settings.API_INTERNAL_PREFIX}"
 
 app.include_router(city_router, prefix=api_prefix)
+app.include_router(cities_router, prefix=api_prefix)
 app.include_router(itinerary_router, prefix=api_prefix)
 app.include_router(search_router, prefix=api_prefix)
 app.include_router(internal_router, prefix=internal_prefix)
