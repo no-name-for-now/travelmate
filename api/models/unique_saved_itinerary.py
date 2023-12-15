f3c592fb14df8d8674debdc80afc0f774a4dd9c8
Karel Verhoeven <karelverhoeven@Karels-Mac-mini.local>
2023-12-15 16:50:45 +0200

    new model hack

--- api/models/unique_saved_itinerary.py
+++ api/models/unique_saved_itinerary.py
@@ -12,6 +12,10 @@ class UniqueSavedItineraryORM(AbstractBaseModel):
 
     user_id = models.IntegerField()
     blob = models.JSONField()
+    city = models.CharField(null=True)
+    country = models.CharField(null=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
 
 
 class UniqueSavedItineraryContract(BaseModel):
@@ -19,6 +23,10 @@ class UniqueSavedItineraryContract(BaseModel):
 
     user_id: int
     blob: Any
+    city: str
+    country: str
+    start_date: str
+    end_date: str
 
     @classmethod
     def from_model(
@@ -27,8 +35,12 @@ class UniqueSavedItineraryContract(BaseModel):
         """Convert a Django CityClimate model instance to an APICityClimate instance."""
         return cls(
             id=instance.id,
-            user_id=instance.city_id,
+            user_id=instance.user_id,
             blob=instance.blob,
+            city=instance.city,
+            country=instance.country,
+            start_date=instance.start_date,
+            end_date=instance.end_date,
         )
 
     def to_dict(self):
@@ -36,4 +48,8 @@ class UniqueSavedItineraryContract(BaseModel):
             "id": self.id,
             "user_id": self.user_id,
             "blob": self.blob,
+            "city": self.city,
+            "country": self.country,
+            "start_date": self.country,
+            "end_date": self.country,
         }
